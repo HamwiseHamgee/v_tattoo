54f611ab831f52e6e7feafb095f2581f9113a6fa
Sam <sgphillips8@gmail.com>
2023-03-08 15:32:55 -0500

    Started setting up app

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>WEBSITE FINISHED</h1>
+        <h2>MISSION ACCOMPLISHED</h2>
+        <h2>GOOD JOB, EVERYONE</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
