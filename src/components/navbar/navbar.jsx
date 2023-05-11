efbac33ffab43adf8b6fc227c3448036ba52e2d3
Sam <sgphillips8@gmail.com>
2023-05-11 17:16:47 -0400

    Reconfigured navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,7 +4,6 @@ import hamburger_icon from '../../assets/images/hamburger_icon.svg';
 import instagram_icon from '../../assets/images/instagram_icon.svg';
 
 const Navbar = () => {
-
 	const [showNav, setShowNav] = useState(false);
 
 	const navClick = () => {
@@ -13,6 +12,62 @@ const Navbar = () => {
 		} else {
 			setShowNav(false);
 		}
+	};
+
+	const navScroll = (e) => {
+		const clickedName = e.currentTarget.id;
+
+		let targetName = '';
+
+		if (clickedName === 'navSplashpage') {
+			targetName = 'splashpageContainer';
+		} else if (clickedName === 'navGallery') {
+			targetName = 'galleryContainer';
+		} else if (clickedName === 'navBooking') {
+			targetName = 'bookingContainer';
+		} else if (clickedName === 'navBio') {
+			targetName = 'bioContainer';
+		}
+
+		document
+			.getElementById(targetName)
+			.scrollIntoView({ behavior: 'smooth' });
+
+		navClick();
+	};
+
+	const NavContent = () => {
+		return (
+			<nav id='navContent'>
+				<ol>
+					<li
+						onClick={navScroll}
+						className='navLink'
+						id='navSplashpage'
+					>
+						Home
+					</li>
+					<li onClick={navScroll} className='navLink' id='navGallery'>
+						Gallery
+					</li>
+					<li onClick={navScroll} className='navLink' id='navBooking'>
+						Booking
+					</li>
+					<li onClick={navScroll} className='navLink' id='navBio'>
+						Bio
+					</li>
+					<li className='navLink' id='navInsta'>
+						<a href='https://www.instagram.com/v.ffoxx/'>
+							<img
+								src={instagram_icon}
+								alt='Instagram Icon'
+								id='instaIcon'
+							></img>
+						</a>
+					</li>
+				</ol>
+			</nav>
+		);
 	};
 
 	return (
@@ -24,60 +79,6 @@ const Navbar = () => {
 			</span>
 			{showNav ? <NavContent /> : null}
 		</div>
-	);
-};
-
-const navScroll = (e) => {
-
-	
-
-	const clickedName = e.currentTarget.id;
-
-	let targetName = '';
-
-	if (clickedName === 'navSplashpage'){
-		targetName = 'splashpageContainer';
-	} else if (clickedName === 'navGallery') {
-		targetName = 'galleryContainer';
-	} else if (clickedName === 'navBooking') {
-		targetName = 'bookingContainer';
-	} else if (clickedName === 'navBio') {
-		targetName = 'bioContainer';
-	};
-
-
-	document.getElementById(targetName).scrollIntoView({ behavior: 'smooth' });
-
-};
-
-const NavContent = () => {
-
-	return (
-		<nav id='navContent'>
-			<ol>
-				<li onClick={navScroll} className='navLink' id='navSplashpage'>
-					Home
-				</li>
-				<li onClick={navScroll} className='navLink' id='navGallery'>
-					Gallery
-				</li>
-				<li onClick={navScroll} className='navLink' id='navBooking'>
-					Booking
-				</li>
-				<li onClick={navScroll} className='navLink' id='navBio'>
-					Bio
-				</li>
-				<li className='navLink' id='navInsta'>
-					<a href='https://www.instagram.com/v.ffoxx/'>
-						<img
-							src={instagram_icon}
-							alt='Instagram Icon'
-							id='instaIcon'
-						></img>
-					</a>
-				</li>
-			</ol>
-		</nav>
 	);
 };
 
